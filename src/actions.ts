101ccc76a8427d5eb50618ff9e7d211488b6ff06
jahill <jahill@groupon.com>
2022-12-06 16:51:20 +0000

    feat: working poc of selective bloom

--- src/actions.ts
+++ src/actions.ts
@@ -1,5 +1,19 @@
-import { Clock, Color, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import {
+  Clock,
+  Color,
+  Layers,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  ShaderMaterial,
+  Vector2,
+  WebGLRenderer,
+} from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 import { spaceBox } from './scene/spaceBox.scene';
 import { glowingStar, updateGlowingStar } from './scene/glowingStar.scene';
 import { sunLight } from './scene/sunLight.scene';
@@ -19,6 +33,27 @@ let camera: PerspectiveCamera;
 let controls: OrbitControls;
 let isPaused = false;
 
+// TEST
+let bloomComposer;
+let bloomLayer;
+let finalComposer;
+let materials = {};
+const darkMaterial = new MeshBasicMaterial({ color: 'black' });
+
+function darkenNonBloomed(obj) {
+  if (obj.isMesh && bloomLayer.test(obj.layers) === false) {
+    materials[obj.uuid] = obj.material;
+    obj.material = darkMaterial;
+  }
+}
+
+function restoreMaterial(obj) {
+  if (materials[obj.uuid]) {
+    obj.material = materials[obj.uuid];
+    delete materials[obj.uuid];
+  }
+}
+
 /* -----------------------------------
  *
  * Setup
@@ -37,6 +72,8 @@ function setup(canvas: HTMLCanvasElement) {
   controls.minDistance = 25;
   controls.maxDistance = 750;
 
+  glowingStar.layers.enable(1);
+
   scene.background = new Color('black');
   scene.add(spaceBox);
   scene.add(glowingStar);
@@ -46,6 +83,70 @@ function setup(canvas: HTMLCanvasElement) {
   renderer = new WebGLRenderer({ canvas });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
+
+  // BLOOM
+  // --------------------------------
+  bloomLayer = new Layers();
+  bloomLayer.set(1);
+
+  const renderPass = new RenderPass(scene, camera);
+  const bloomPass = new UnrealBloomPass(
+    new Vector2(window.innerWidth, window.innerHeight),
+    1.5,
+    0.4,
+    0.85
+  );
+
+  bloomPass.threshold = 0;
+  bloomPass.strength = 5;
+  bloomPass.radius = 0;
+
+  bloomComposer = new EffectComposer(renderer);
+
+  bloomComposer.renderToScreen = false;
+  bloomComposer.addPass(renderPass);
+  bloomComposer.addPass(bloomPass);
+
+  const shaderPass = new ShaderPass(
+    new ShaderMaterial({
+      uniforms: {
+        baseTexture: { value: null },
+        bloomTexture: { value: bloomComposer.renderTarget2.texture },
+      },
+      vertexShader: `
+      varying vec2 vUv;
+
+      void main() {
+
+          vUv = uv;
+
+          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }`,
+      fragmentShader: `
+      uniform sampler2D baseTexture;
+			uniform sampler2D bloomTexture;
+
+			varying vec2 vUv;
+
+			void main() {
+
+				gl_FragColor = ( texture2D( baseTexture, vUv ) + vec4( 1.0 ) * texture2D( bloomTexture, vUv ) );
+
+			}
+      `,
+      defines: {},
+    }),
+    'baseTexture'
+  );
+
+  shaderPass.needsSwap = true;
+
+  finalComposer = new EffectComposer(renderer);
+
+  finalComposer.addPass(renderPass);
+  finalComposer.addPass(shaderPass);
+  // --------------------------------
+  // END
 
   window.addEventListener('resize', () => onResize(resize), false);
 }
@@ -81,7 +182,12 @@ function stop() {
 function animate() {
   const delta = clock.getDelta();
 
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+
+  scene.traverse(darkenNonBloomed);
+  bloomComposer.render();
+  scene.traverse(restoreMaterial);
+  finalComposer.render();
 
   if (isPaused) {
     return;
@@ -90,8 +196,6 @@ function animate() {
   requestAnimationFrame(animate);
   updateGlowingStar();
   updateSpinningPlanet({ delta });
-
-  console.log(camera.position);
 }
 
 /* -----------------------------------
