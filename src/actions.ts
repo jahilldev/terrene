f4598ce9f9d743226616b22d65e91ebdfd534595
jahill <jahill@groupon.com>
2022-12-06 16:17:40 +0000

    feat: added poc bloom effect

--- src/actions.ts
+++ src/actions.ts
@@ -1,5 +1,8 @@
-import { Clock, Color, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { Clock, Color, PerspectiveCamera, Scene, Vector2, WebGLRenderer } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 import { spaceBox } from './scene/spaceBox.scene';
 import { glowingStar, updateGlowingStar } from './scene/glowingStar.scene';
 import { sunLight } from './scene/sunLight.scene';
@@ -15,6 +18,7 @@ import { onResize } from '@/utility/canvasHelper.utility';
 const scene = new Scene();
 const clock = new Clock();
 let renderer: WebGLRenderer;
+let composer: EffectComposer;
 let camera: PerspectiveCamera;
 let controls: OrbitControls;
 let isPaused = false;
@@ -46,6 +50,26 @@ function setup(canvas: HTMLCanvasElement) {
   renderer = new WebGLRenderer({ canvas });
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
+
+  // TEST
+  const renderScene = new RenderPass(scene, camera);
+  const bloomPass = new UnrealBloomPass(
+    new Vector2(window.innerWidth, window.innerHeight),
+    1.5,
+    0.4,
+    0.85
+  );
+
+  bloomPass.threshold = 0;
+  bloomPass.strength = 1; // intensity of glow
+  bloomPass.radius = 0;
+
+  composer = new EffectComposer(renderer);
+  composer.setSize(window.innerWidth, window.innerHeight);
+  composer.renderToScreen = true;
+  composer.addPass(renderScene);
+  composer.addPass(bloomPass);
+  // END
 
   window.addEventListener('resize', () => onResize(resize), false);
 }
@@ -81,7 +105,8 @@ function stop() {
 function animate() {
   const delta = clock.getDelta();
 
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+  composer.render();
 
   if (isPaused) {
     return;
@@ -90,8 +115,6 @@ function animate() {
   requestAnimationFrame(animate);
   updateGlowingStar();
   updateSpinningPlanet({ delta });
-
-  console.log(camera.position);
 }
 
 /* -----------------------------------
